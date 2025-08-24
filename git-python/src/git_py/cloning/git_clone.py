a7d8ae46edafadfa7e9319572c4e31a57752230f
malamg <mohammadmahfooz.alam@gainwelltechnologies.com>
2025-08-24 07:46:52 -0700

    git python

--- git-python/src/git_py/cloning/git_clone.py
+++ git-python/src/git_py/cloning/git_clone.py
@@ -1,15 +1,15 @@
-from pathlib import Path
-from git import Repo
 import os
+from pathlib import Path
+
+from git import Repo
+
 
 def main():
     repository_folder = "JavaPro"
     repository_path = Path(os.environ["DATA_HOME"]) / "repository"
     local_dir = repository_path / repository_folder
-    
-    Repo.clone_from(
-        "https://github.com/official-himanshu/JavaPro.git", local_dir
-    )
+
+    Repo.clone_from("https://github.com/official-himanshu/JavaPro.git", local_dir)
 
     # Open an existing local repo
     repo = Repo(local_dir)
